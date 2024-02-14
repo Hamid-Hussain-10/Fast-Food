1da0277cc607b0e9fa074ab55225607ef2fa6ea0
Hamid Hussain <146005938+03554718903@users.noreply.github.com>
2024-02-14 11:28:11 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+function imgSlider1(imageSrc) {
+    document.getElementById('mainImage').src = imageSrc;
+}   
+
+function imgSlider(imageSrc) {
+    document.getElementById('contactImage').src = imageSrc;
+}   
+
+  
